042dbede031f071906dc4a1c48c5d4ad565ef6f4
Furkan <76282993+FurkanBeyazit@users.noreply.github.com>
2024-08-03 12:35:31 +0900

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
-# TinyLlama model ve tokenizer'ı yükleyin
+
 model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-# Pipeline'ı oluşturun
+
 pipe = pipeline(
     "text-generation", 
     model=model, 
@@ -16,22 +16,21 @@ pipe = pipeline(
     repetition_penalty=1.2
 )
 
-# Streamlit Uygulaması
 st.title("Top 3 Brand")
 st.write("Top 3 Brand of the Product")
 
-# Kullanıcıdan mutfak türü seçimi al
-cuisine_options = ["Watch", "Art", "Sport Car","Fashion bag",]
-selected_cuisine = st.selectbox("Select a cuisine type:", cuisine_options)
 
-# Kullanıcıdan "Get Recommendations" butonuna basmasını bekle
+options = ["Watch", "Art", "Sport Car","Fashion bag",]
+selected = st.selectbox("Select a type:",options)
+
+
 if st.button("Check "):
-    # Kullanıcıdan prompt oluştur
-    prompt = f"Most expensive {selected_cuisine} brands top 3  "
+   
+    prompt = f"Most expensive {selected} brands top 3  "
 
-    # Modelden yanıt al
+    
     response = pipe(prompt)
 
-    # Sonuçları yazdır
+    
     st.write("Top-3:")
     st.write(response[0]['generated_text'])
